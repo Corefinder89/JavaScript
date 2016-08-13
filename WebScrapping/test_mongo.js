f6f69577b4c871d9ed529375ccc5626f7c8cb113
Soumyajit Basu <soumyajit.basu@srijan.in>
2016-08-13 20:48:47 +0530

    Delete test_mongo.js

--- WebScrapping/test_mongo.js
+++
@@ -1,8 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var url = 'mongodb://localhost:27017/db';
-MongoClient.connect(url,function(err,db){
-	assert.equal(null,err);
-	console.log('Connected correctly to server');
-	db.close();
-});
